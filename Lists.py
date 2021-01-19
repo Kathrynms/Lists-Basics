eee6653c1d8dbe0fb38a1c9605ab6396abb84f65
Kathryn So <77600634+Kathrynms@users.noreply.github.com>
2021-01-19 14:44:24 -0700

    Lists
    Learn Python - Full Course for Beginners 
    Follow along practice

---
+++ Lists.py
@@ -0,0 +1,13 @@
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+friends[1] = "Mike"
+
+print(friends)
+print(friends[0])
+print(friends[2])
+print(friends[-1])
+print(friends[-2])
+print(friends[1:])
+print(friends[1:3])
+
+print(friends[1])
